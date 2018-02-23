f4e9f43baa2aa921167cb61e83c2fa27af70b445
Rowin ten Hoope <rowin939@hotmail.com>
2018-02-23 04:19:32 +0100

    can now read all music files anywhere on cellphone

--- app/src/main/java/com/example/rowin/urchinmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/rowin/urchinmusicplayer/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.rowin.urchinmusicplayer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,12 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AudioManager manager = new AudioManager(this);
-        manager.getSongsList();
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
     }
 
-    private void readMusicFiles(){
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission granted and now can proceed
+                     //a sample method called
+                    AudioManager manager = new AudioManager(this);
+                    manager.Loadmusic();
+                } else {
 
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            // add other cases for more permissions
+        }
     }
 }
 
