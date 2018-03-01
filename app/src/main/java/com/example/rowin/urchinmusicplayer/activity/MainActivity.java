63ce2eebb2a6573dde8656c1d29239196d331198
Rowin <rowin939@hotmail.com>
2018-03-01 03:47:56 +0100

    Added a tab for currently playing son, also added an animation to the play/pause button.

--- app/src/main/java/com/example/rowin/urchinmusicplayer/activity/MainActivity.java
+++ app/src/main/java/com/example/rowin/urchinmusicplayer/activity/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.rowin.urchinmusicplayer.activity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -13,6 +19,10 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.rowin.urchinmusicplayer.R;
@@ -29,18 +39,55 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<Song> listOfSongs;
+    private ArrayList<Song> listOfSongs;
+    private ImageView playButton;
+    private Boolean musicIsPlaying = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initializeViews();
 
         ActivityCompat.requestPermissions(MainActivity.this,
                 new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        playButtonOnClick();
+    }
+
+    //TODO change view name imageButton to something more appropriate
+    private void initializeViews(){
+        playButton = findViewById(R.id.imageButton);
+    }
+
+    private void playButtonOnClick(){
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View view) {
+                if(!musicIsPlaying){
+                    playButton.setImageResource(R.drawable.play_to_pause_animator);
+                    Drawable playToPauseAnimation = playButton.getDrawable();
+                    startAnimation(playToPauseAnimation);
+                    musicIsPlaying = true;
+                } else {
+                    playButton.setImageResource(R.drawable.pause_to_play_animator);
+                    Drawable pauseToPlayAnimation = playButton.getDrawable();
+                    startAnimation(pauseToPlayAnimation);
+                    musicIsPlaying = false;
+                }
+
+            }
+        });
+    }
+
+    private void startAnimation(Drawable drawable){
+        if(drawable instanceof Animatable){
+            ((Animatable) drawable).start();
+        }
     }
 
     private void setAdapterForViewPager(){
