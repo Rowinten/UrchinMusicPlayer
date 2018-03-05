69cf800419e432707d574983b2e3afbbd94a8888
Rowin <rowin939@hotmail.com>
2018-03-05 02:10:18 +0100

    songs do not crash anymore after the first played song, was due to the Runnable not being called to stop when a new song was played.

--- app/src/main/java/com/example/rowin/urchinmusicplayer/fragment/SongListFragment.java
+++ app/src/main/java/com/example/rowin/urchinmusicplayer/fragment/SongListFragment.java
@@ -1,8 +1,6 @@
 package com.example.rowin.urchinmusicplayer.fragment;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.MediaPlayer;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,7 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 
 import com.example.rowin.urchinmusicplayer.R;
 import com.example.rowin.urchinmusicplayer.activity.MainActivity;
@@ -20,9 +18,8 @@ import com.example.rowin.urchinmusicplayer.adapter.RecyclerViewAdapter;
 import com.example.rowin.urchinmusicplayer.model.Globals;
 import com.example.rowin.urchinmusicplayer.model.Song;
 import com.example.rowin.urchinmusicplayer.util.Animations;
+import com.example.rowin.urchinmusicplayer.util.SongManager;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -36,11 +33,9 @@ public class SongListFragment extends Fragment{
     private RecyclerViewAdapter.ViewHolder previouslyTabbedItemView;
     private Animations animations;
 
-    private ImageView playButton, frontAlbumImage, backAlbumImage;
-    private TextView songTitleView, songBandView;
-    private Boolean isAlbumBackVisible = false;
-
-    private View albumBack, albumFront;
+    private ImageView playButton;
+    private ProgressBar songProgressBar;
+    private SongManager songManager;
 
 
 
@@ -54,6 +49,7 @@ public class SongListFragment extends Fragment{
         View view =  inflater.inflate(R.layout.songs_tab_fragment, container, false);
         initializeViews(view);
 
+        songManager = new SongManager(getContext());
         Bundle bundle = this.getArguments();
         ArrayList<Song> listOfSongs = bundle.getParcelableArrayList("listOfSongs");
         animations = new Animations(getContext());
@@ -68,12 +64,7 @@ public class SongListFragment extends Fragment{
     private void initializeViews(View view){
         songListRecyclerView = view.findViewById(R.id.songRecyclerView);
         playButton = ((MainActivity) getActivity()).playButton;
-        frontAlbumImage = ((MainActivity)getActivity()).frontAlbumCoverView;
-        backAlbumImage = ((MainActivity)getActivity()).backAlbumCoverView;
-        songTitleView = ((MainActivity)getActivity()).songTitleView;
-        songBandView = ((MainActivity)getActivity()).songBandView;
-        albumBack = ((MainActivity) getActivity()).backAlbumCoverLayout;
-        albumFront = ((MainActivity) getActivity()).frontAlbumCoverLayout;
+        songProgressBar = ((MainActivity) getActivity()).audioProgressBar;
     }
 
 
@@ -82,8 +73,9 @@ public class SongListFragment extends Fragment{
             @Override
             public void onItemClick(RecyclerViewAdapter.ViewHolder holder, Song song) {
                 changeItemViewTitleColor(holder);
-                changeAlbumCoverPicture(getImageCoverFromMusicFile(song.getAlbumCoverPath()));
-                setSongVariablesInTextViews(song);
+                Bitmap imageCover = songManager.getAlbumCoverFromMusicFile(song.getAlbumCoverPath());
+                songManager.changeAlbumCoverPicture(imageCover);
+                songManager.setSongVariablesInTextViews(song);
 
                 //If no Music is playing, change play icon to pause icon when song is clicked
                 //If song is playing then do nothing for the animation. since the pause button is already visible.
@@ -92,38 +84,9 @@ public class SongListFragment extends Fragment{
                     Globals.isMusicPlaying = true;
                 }
 
-                playSelectedSong(song.getSongPath());
-
+                songManager.playSelectedSongSequence(song.getSongPath(), songProgressBar);
             }
         });
-    }
-
-    //Currently_playing_song_tab has a FrameLayout containing back and front side of an ImageView ( actually two ImageViews in FrameLayout ) back shows first in app.
-    //when clicked an animation plays that flips over to the opposite ImageView and displays the album cover of the newly clicked song
-    //isAlbumBackVisible keeps record of which side is on the visible side.
-    private void changeAlbumCoverPicture(Bitmap newAlbumCover){
-        if(!isAlbumBackVisible){
-            backAlbumImage.setImageBitmap(newAlbumCover);
-            animations.backToFrontAnimation(albumBack, albumFront);
-            isAlbumBackVisible = true;
-        } else {
-            frontAlbumImage.setImageBitmap(newAlbumCover);
-            animations.frontToBackAnimation(albumFront, albumBack);
-            isAlbumBackVisible = false;
-        }
-    }
-
-    private Bitmap getImageCoverFromMusicFile(String filePath){
-        File image = new File(filePath);
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-
-        return BitmapFactory.decodeFile(image.getAbsolutePath(),bmOptions);
-    }
-
-    //Sets the song title and band name in the TextViews from the currently_playing_song_tab.xml
-    private void setSongVariablesInTextViews(Song song){
-        songTitleView.setText(song.getSongName());
-        songBandView.setText(song.getArtist());
     }
 
     private void changeItemViewTitleColor(RecyclerViewAdapter.ViewHolder holder){
@@ -142,30 +105,5 @@ public class SongListFragment extends Fragment{
         previouslyTabbedItemView = currentlyTabbedItemView;
     }
 
-    private void playSelectedSong(String songPath){
-        //gets subclass of Application for global variable globalsSong
-        Globals globals = ((Globals) getContext().getApplicationContext());
-        //Checks if there was a song playing before the currently clicked song and stops it.
-        if(globals.getCurrentlyPlayingSong() != null){
-            MediaPlayer previousPlayingSong = globals.getCurrentlyPlayingSong();
-            previousPlayingSong.reset();
-            previousPlayingSong.release();
-        }
 
-        //TODO Solve error after tapping songs quickly (-19, 0) error
-        //Starts new MediaPlayer with the current clicked song
-        MediaPlayer mediaPlayer = new MediaPlayer();
-        try {
-            mediaPlayer.setDataSource(songPath);
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //sets the new clicked song as the currently playing song
-        globals.setCurrentlyPlayingSong(mediaPlayer);
-        ((MainActivity)getActivity()).syncProgressBarWithAudioDuration(mediaPlayer);
-
-    }
 }
